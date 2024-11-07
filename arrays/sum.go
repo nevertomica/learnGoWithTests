79d5d9b678293461e01a7c97fdf9ca5af5f7ef3b
nevertomica <scheepyang@gmail.com>
2024-11-07 16:37:27 +0800

    [refactor] 更新arrays 模組  將SumAll內部改用append方法添加元素  新增SumAllTrails: 除去slice第一個元素的總和

--- arrays/sum.go
+++ arrays/sum.go
@@ -2,18 +2,21 @@ package arrays
 
 // 將欲加總的數放進同一個 slices，並傳入 SumAll 函式，就可以得到總和
 // 不過總和會是以一個 slice 回傳
+// func 透過初始一個 empty slice，後續透過 append 加添元素
 func SumAll(numberArrays ...[]int) []int {
 
 	arrayCapability := len(numberArrays)
-	sumArray := make([]int, arrayCapability)
+	sumArray := make([]int, 0, arrayCapability)
 
-	for i, numbers := range numberArrays {
-		sumArray[i] = Sum(numbers)
+	for _, numbers := range numberArrays {
+		// 只要不超過 sumArray 底層陣列長度，都會指向同一個底層陣列
+		sumArray = append(sumArray, Sum(numbers))
 	}
 
 	return sumArray
 }
 
+// 加總 slice 中的所有元素
 func Sum(numbers []int) int {
 	var sum int
 
@@ -22,4 +25,19 @@ func Sum(numbers []int) int {
 	}
 
 	return sum
+}
+
+// SumAll 的改良，加總每一個除去第一個元素的 slice 總和
+// 如果傳入 []int{3} 加總是 []int{0}，另外如果是空的 slice 也是回傳 []int{0}
+func SumAllTails(slices ...[]int) []int {
+	subSlice := make([][]int, 0, len(slices))
+	for _, arr := range slices {
+		if len(arr) > 1 {
+			subSlice = append(subSlice, arr[1:])
+		} else {
+			subSlice = append(subSlice, []int{})
+		}
+	}
+	return SumAll(subSlice...)
+
 }
--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -26,3 +26,42 @@ func TestSum(t *testing.T) {
 		t.Errorf("got %d want %d, given %v", got, want, numbers)
 	}
 }
+
+func TestSumAllTails(t *testing.T) {
+
+	checkSum := func(t testing.TB, got, want []int, input ...[]int) {
+		t.Helper()
+		if !slices.Equal(want, got) {
+			t.Errorf("got %d want %d, given %v", got, want, input)
+		}
+
+	}
+
+	t.Run(" len = 1 subslice ", func(t *testing.T) {
+		numbers := []int{3}
+		want := []int{0}
+		got := SumAllTails(numbers)
+		checkSum(t, got, want, numbers)
+	})
+
+	t.Run("len = 0 slice", func(t *testing.T) {
+		numbers := []int{}
+		want := []int{0}
+		got := SumAllTails(numbers)
+		checkSum(t, got, want, numbers)
+	})
+
+	t.Run(" len > 1 subslice ", func(t *testing.T) {
+		numbers := []int{1, 2, 3, 4, 5}
+		want := []int{14}
+		got := SumAllTails(numbers)
+		checkSum(t, got, want, numbers)
+	})
+
+	t.Run(" mixed subslices ", func(t *testing.T) {
+		input := [][]int{[]int{1, 2, 3, 4, 5}, []int{2, 3}, []int{100}, []int{}}
+		want := []int{14, 3, 0, 0}
+		got := SumAllTails(input...)
+		checkSum(t, got, want, input...)
+	})
+}
